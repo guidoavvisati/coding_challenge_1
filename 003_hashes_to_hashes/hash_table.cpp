f50ce09eca9b31dd3cfea2edc4fa53b8bea37c38
Guido Avvisati <guido.avvisati@gmail.com>
2020-10-24 18:35:27 +0200

    implement add method

--- 003_hashes_to_hashes/hash_table.cpp
+++ 003_hashes_to_hashes/hash_table.cpp
@@ -7,28 +7,39 @@
 
 // Node struct for linked list
 typedef struct Node {
-  struct Node* next;
-  char*  key;
-  char* value;
+  struct Node *next;
+  char *key;
+  char *value;
 
+  // Initialize with NULL
   Node() :
-    next(nullptr), key(nullptr), value(nullptr){};
+    next(nullptr), key(nullptr), value(nullptr) {};
+
+  // Initialize memory, pass refs, copy values
+  Node(const char *_key, const char *_value, Node *_next) :
+    next(_next), key(nullptr), value(nullptr) {
+    key = new char[strlen(_key)];
+    value = new char[strlen(_value)];
+    strcpy(key, _key);
+    strcpy(value, _value);
+  };
+  
+
 } Node;
 
-// Hash Table setup, array of pointers
+// Hash Table setup, array of references
 typedef struct TableManager {
-  Node** table;
+  Node **table;
   size_t table_size;
+  uint32_t table_entry_counts;
 
-  TableManager(size_t table_size=TABLE_LEN) : table_size(table_size), table(nullptr) {
-    table = new Node*[table_size];
-    for (size_t i = 0; i < table_size; ++i) {
-      table[i] = new Node();
-    }
+  TableManager(size_t table_size=TABLE_LEN) :
+    table_size(table_size), table(nullptr), table_entry_counts(0) {
+    table = new Node * [table_size]();
   };
 
   // Hash methods
-  uint32_t murmur_hash_32(const char* key)
+  uint32_t murmur_hash_32(const char *key)
   {
     uint32_t hash(3323198485ul);
     for (;*key;++key) {
@@ -39,16 +50,46 @@ typedef struct TableManager {
     return hash;
   }
 
-  uint32_t get_hash(const char* key){
+  uint32_t get_hash(const char *key){
     uint32_t idx = murmur_hash_32(key);
     return idx % this->table_size;
   }
+
+  // API
+  bool add(const char *key, const char *value){
+    table_entry_counts++;
+    uint32_t idx = get_hash(key);
+
+    if(table[idx] == nullptr){ // Case no collisions
+      table[idx] = new Node(key, value, nullptr);
+    }
+    else{ // Case with collision, prepend new to list
+      table[idx] = new Node(key, value, table[idx]);
+    }
+
+    return true;
+  }
+
+  Node *find(const char *key){
+    
+    return NULL;
+  }
+
+  bool remove(Node *node){
+    return 1;
+  }
+
+  
 } TableManager;
 
 
 
-int main(){
+int main(int argc, char *argv[]){
+  
   TableManager mgr = TableManager(100);
   Node** table = mgr.table;
-  printf("%s\n", table[0]->key);
+  if(argc > 1) printf("%d\n", mgr.get_hash(argv[1]));
+  mgr.add("123", "ab");
+  mgr.add("123", "mini");
+  printf("%d\n", mgr.table_entry_counts);
 }
