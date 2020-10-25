763440e5f041bb21c486a9267ebed2a6cec4926f
Guido Avvisati <guido.avvisati@gmail.com>
2020-10-25 19:02:53 +0100

    added main with small ex

--- 002_under_the_heap/allocator.cpp
+++ 002_under_the_heap/allocator.cpp
@@ -2,6 +2,9 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdint>
+#include <unistd.h>
+
+# define MAX_MEMORY 32768
 
 typedef struct header_t {
   header_t *next;
@@ -9,25 +12,32 @@ typedef struct header_t {
   char is_free;
 } header_t;
 
-header_t *first = NULL, *last = NULL;
+header_t *head = NULL, *tail = NULL;
 
-header_t *get_free_block(header_t *first, size_t size) {
-  header_t *curr = first;
+void set_block(header_t *block, char is_free, size_t size, header_t *next){
+  block->next = next;
+  block->is_free = is_free;
+  block->size = size;
+}
+
+header_t *get_free_block(size_t size) {
+  header_t *curr = head;
+  // Go over the list until block of suitable size is found
   while(curr){
     if (curr->is_free && curr->size >= size)
       return curr;
     curr = curr->next;
   }
-  return NULL;
+  return nullptr;
 }
 
-void *custom_malloc(header_t *first, size_t size){
+void *custom_malloc(size_t size){
   size_t total_size;
   void *block;
   header_t *header;
 
-  if (!size) return NULL;
-  header = get_free_block(first, size);
+  if (!size) return nullptr;
+  header = get_free_block(size);
   if (header) {
     header->is_free = 0;
     return (void*)(header + 1);
@@ -36,20 +46,27 @@ void *custom_malloc(header_t *first, size_t size){
   total_size = sizeof(header_t) + size;
   block = sbrk(total_size);
   if (block == (void*) -1) {
-    return NULL;
+    return nullptr;
   }
 
-  header = block;
-  header->size = size;
-  header->is_free = 0;
-  header->next = NULL;
-  if (!first)
-    first = header;
-  if (last)
-    last->next = header;
-  last = header;
+  header = (header_t *)block;
+  set_block(header, 0, size, nullptr);
+
+  // Update head and tail
+  if (!head)
+    head = header;
+  if (tail)
+    tail->next = header;
+  tail = header;
   return (void*)(header + 1);
 }
 
+int main(int argc, char *argv[]){
+  char* my_str1 = (char*)custom_malloc(7);
+  strncpy(my_str1, "Hello ", 7);
+  char* my_str2 = (char*)custom_malloc(7);
+  strncpy(my_str2, "Hello ", 7);
+  printf("%s %s\n", my_str1, my_str2);
 
+}
 
